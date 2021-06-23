f883a3ab031d8ee9731db1ae5840a1adf27d0842
Anton Degnera <antilik@gmail.com>
2021-06-24 00:34:26 +0300

    setup and preparation for testing

---
+++ src/components/CommentBox.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class CommentBox extends Component {
+  state = { comment: "" };
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+    console.log("state >>>>>>>>>>", this.state);
+  };
+
+  handleSubmit = (event) => {
+    event.PreventDefault();
+
+    // TODO: call action saving to Redux
+
+    this.setState({ comment: "" });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea value={this.state.comment} onChange={this.handleChange} />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CommentBox;
---
+++ src/components/__tests__/CommentBox.test.js
@@ -0,0 +1,11 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import CommentBox from "components/CommentBox";
+
+it("shows a comment box", () => {
+  //   const div = document.createElement("div");
+  //   ReactDOM.render(<CommentBox />, div);
+  //   expect(div.innerHTML).toContain("Comment Box");
+  //   ReactDOM.unmountComponentAtNode(div);
+});
