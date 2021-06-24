2f08629bf381bfa8eb569333c9344f04ead92753
Anton Degnera <antilik@gmail.com>
2021-06-25 00:17:44 +0300

    simulating change and submit events, etc

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -7,6 +7,7 @@ import CommentBox from "components/CommentBox";
 
 describe("<CommonBox />", () => {
   let wrapped = null;
+  const myComment = "My new comment";
 
   beforeEach(() => {
     wrapped = mount(<CommentBox />);
@@ -23,10 +24,27 @@ describe("<CommonBox />", () => {
   });
 
   it("can enter input into the textarea", () => {
-    //
+    wrapped.find("textarea").simulate("change", {
+      target: { value: myComment },
+    });
+    wrapped.update();
+
+    expect(wrapped.find("textarea").prop("value")).toEqual(myComment);
   });
 
-  it("when input is submitted, textarea should get emptied", () => {
-    //
+  it("when form is submitted, textarea should get emptied", () => {
+    expect(wrapped.find("form").length).toEqual(1);
+
+    wrapped
+      .find("textarea")
+      .simulate("change", { target: { value: myComment } });
+
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual(myComment);
+
+    wrapped.find("form").simulate("submit");
+
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
   });
 });
